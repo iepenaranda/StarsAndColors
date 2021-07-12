c072f2fecf555755f33164277158aaa1bdf5fed1
iepenaranda <iepo960709@gmail.com>
2021-07-12 12:36:53 -0500

    Colors and stars

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   return (
     <div>
       <StarRating />
-
       <ColorList
         colors={colors}
         onRemoveColor={(id) => {
--- src/Color.js
+++ src/Color.js
@@ -1,21 +1,15 @@
 import React from "react";
 import { FaTrash } from "react-icons/fa";
 
-export default function Color({
-  id,
-  title,
-  color,
-  rating,
-  onRemove = (id) => id,
-}) {
-  return (
-    <section>
-      <h1>{title}</h1>
-      <button onClick={() => onRemove(id)}>
-        {" "}
-        <FaTrash />{" "}
-      </button>
-      <div style={{ height: 50, backgroundColor: color }}></div>
-    </section>
-  );
+
+export default function Color({id, title, color, rating, onRemove = id => id}) {
+    return (
+        <section>
+            <h1>{title}</h1>
+            <button onClick={() => onRemove(id)}> <FaTrash/> </button>
+            <div style={{height: 50, backgroundColor: color}}>
+
+            </div>
+        </section>
+    );
 }
